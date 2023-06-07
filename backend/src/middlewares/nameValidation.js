e22559683a553a9964afc4794ee10b9ca41409cc
Lucas <lm.spizzirri@gmail.com>
2023-06-07 18:09:15 -0300

    Validação do Post sem testes

---
+++ backend/src/middlewares/nameValidation.js
@@ -0,0 +1,10 @@
+const nameValidation = (req, res, next) => {
+    const { name } = req.body;
+    if (!name) { return res.status(400).json({ messsage: 'name is required' }); }
+    if (name.length < 5) { 
+    return res.status(400).json({ messsage: 'name length must be at least 5 characters long' }); 
+    }
+    next();
+};
+
+module.exports = nameValidation;
--- backend/src/routes/productsRoute.js
+++ backend/src/routes/productsRoute.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 const productsController = require('../controllers/productsController');
+const nameValidation = require('../middlewares/nameValidation');
 
 const productsRoute = Router();
 productsRoute.get('/', productsController.getAll);
 productsRoute.get('/:id', productsController.getById);
-productsRoute.post('/', productsController.create);
+productsRoute.post('/', nameValidation, productsController.create);
 
 module.exports = productsRoute;
